e6f1e5eb075877f13f0657b02ad6381069223d81
Anurag Verma <anurag.premaverma@gmail.com>
2022-07-23 23:56:50 +0530

    added

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -1,25 +1,24 @@
 import React from "react";
 import styles from "./counter.module.css";
-import { useState } from "react";
 
-const Counter = ( props) => {
-  console.log(props)
-  // sample value to be replaced
-  const count = props.data;
-  const [ccount, setcCount] = useState(count)
-  // NOTE: do not delete `data-cy` key value pair
-
-
-
+const Counter = ({ count, updateCount }) => {
   return (
     <div className={styles.counter}>
-      <button data-cy="task-counter-increment-button" onClick={()=>setcCount(ccount+1)}>+</button>
-      <span data-cy="task-counter-value">{ccount}</span>
-      <button data-cy="task-counter-decrement-button"onClick={()=>setcCount(ccount-1)}>-</button>
+      <button
+        data-cy="task-counter-increment-button"
+        onClick={() => updateCount(count + 1)}
+      >
+        +
+      </button>
+      <span data-cy="task-counter-value">{count}</span>
+      <button
+        data-cy="task-counter-decrement-button"
+        onClick={() => updateCount(count - 1)}
+      >
+        -
+      </button>
     </div>
   );
 };
-
-
 
 export default Counter;
